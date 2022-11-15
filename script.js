bde38013ac888e1e8508364b7553d9339a80b3b7
nezrinnecefzade <nezrin.neccefzade@mail.ru>
2022-11-15 23:09:54 +0400

    nazrin

--- script.js
+++ script.js
@@ -1,127 +1,224 @@
-const input = document.querySelector(".comment")
-// task listinin yaradilmasi
-document.querySelector('.dob').onclick = function(){
-    if(document.querySelector('.input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-
-    else{  
-        document.querySelector('#tasks').innerHTML += `
-        <div class="task">
-            <label>
-            <input type="checkbox">
-                <span id="taskname">
-                    ${document.querySelector('.input').value}
-                </span>
-                </label>
-                <button class="delete">x</button>
-            </div>
-        `;
-
-        document.querySelector('.input').value=""
-       let cancel = document.querySelectorAll(".delete");
-        for(let i=0; i<cancel.length; i++){
-            cancel[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-    }
-}
-    
-// delete ve + duymelerinin funksiyasi
-
-let delet=document.querySelector(".x")
-delet.addEventListener("click",()=>{
-    input.classList.add("hide")
-})
-
-let add=document.querySelector(".pl")
-add.addEventListener("click",()=>{
-   if(input.classList.contains("hide")){
-      input.classList.remove("hide")
-   }
-})
-
-
-
-// sort
-
-let duyme=document.querySelectorAll('.duyme')
-let alt=document.querySelector(".alt")
-const tasks = document.querySelector("#tasks")
-
-alt.addEventListener('click', () => {
- alt.style.display="none" 
- ust.style.display="block" 
- 
-let task=document.querySelectorAll('.task')
-let x = [];
-
-task.forEach(ele =>{
-    x.push(ele.firstElementChild.innerText)
-})
-
-tasks.innerHTML=""
-x.sort()
-
-x.forEach(item=>{
-    
-   tasks.innerHTML+= `<div class="task">
-   <label>
-        <input type="checkbox">
-    <span id="taskname">
-        ${item}
-    </span>
-    </label>
-    <button class="delete">x</button>
-</div>`
-
-document.querySelector('.input').value=""
-let cancel = document.querySelectorAll(".delete");
- for(let i=0; i<cancel.length; i++){
-     cancel[i].onclick = function(){
-         this.parentNode.remove();
-     }}
-
-
-})
+let sortImg = document.querySelector(".sortImg img");
+let reverseImg = document.querySelector(".reverseImg img");
+sortImg.addEventListener("mouseover", () => {
+  sortImg.src = "pictures/Group 73.svg";
 });
+sortImg.addEventListener("mouseout", () => {
+  sortImg.src = "pictures/Group 74.svg";
+});
+reverseImg.addEventListener("mouseover", () => {
+  reverseImg.src = "pictures/Group 91.svg";
+});
+reverseImg.addEventListener("mouseout", () => {
+  reverseImg.src = "pictures/Group 90.svg";
+});
+let btnClear = document.querySelector(".clearButton");
+btnClear.addEventListener("mouseover", () => {
+  btnClear.style.background = "purple";
+  btnClear.style.color = "white";
+  btnClear.style.border = "none";
+});
+btnClear.addEventListener("mouseout", (e) => {
+  btnClear.style.background = "white";
+  btnClear.style.color = "#C4C4C4";
+  btnClear.style.border = "1px solid #C4C4C4";
+});
+btnClear.addEventListener("click", () => {
+  inp.value = "";
+});
+let inp = document.querySelector("input");
+let inp_div = document.querySelector(".inputAll");
+let inp_list = document.querySelector(".inputList");
+let btnAdd = document.querySelector(".buttonAdd");
+let ul_div = document.querySelector(".ulDiv");
+let bol = false;
+let a = 0;
+const listItems = [];
+document.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    if(btnAdd.value == ''){
+      ul_div.style.display = "block";
+      p = document.createElement("p");
+      p.innerHTML = inp.value;
+      btn = document.createElement("button");
+      btn.classList.add("deleteButton");
+      btn.innerHTML = btnClear.innerHTML;
+      new_div_li = document.createElement("li");
+      new_div_li.classList.add("liDiv");
+      new_div = document.createElement("div");
+      new_div.classList.add("listText");
+      new_div.append(p);
+      new_div.append(btn);
+      new_div_li.append(new_div);
+      new_div_li.setAttribute("data-index", a);
+      new_div_li.setAttribute("draggable", "true");
+      listItems.push(new_div_li);
+      a += 1;
+      ul_div.append(new_div_li);
+      ul_div.style.padding = "7.52px 0";
+      pInp = document.querySelector(".listText p");
+      if ((inp.value.length / pInp.offsetWidth) * 100 > 14) {
+        p.style.overflowX = "scroll";
+      }
+      inp.value = "";
+      btnAdd.addEventListener("click", () => {
+        inp_list.style.display = "block";
+        inp_list.style.display = "flex";
+        btnClear.style.justifyContent = "center";
+        inp.focus();
+      });
+      if (ul_div.children.length > 5) {
+        ul_div.style.overflowY = "scroll";
+        ul_div.scrollTop = ul_div.scrollHeight;
+        ul_div.style.paddingTop = "5px";
+        ul_div.style.paddingBottom = "0px";
+      }
+      sortImg.addEventListener("click", sortList);
+      deleteList();
+      addDragDrop();
+      styleFunction();
+    }
+    else if (inp.value.trim().length != 0) {
+      ul_div.style.display = "block";
+      p = document.createElement("p");
+      p.innerHTML = inp.value;
+      btn = document.createElement("button");
+      btn.classList.add("deleteButton");
+      btn.innerHTML = btnClear.innerHTML;
+      new_div_li = document.createElement("li");
+      new_div_li.classList.add("liDiv");
+      new_div = document.createElement("div");
+      new_div.classList.add("listText");
+      new_div.append(p);
+      new_div.append(btn);
+      new_div_li.append(new_div);
+      new_div_li.setAttribute("data-index", a);
+      new_div_li.setAttribute("draggable", "true");
+      listItems.push(new_div_li);
+      a += 1;
+      ul_div.append(new_div_li);
+      ul_div.style.padding = "7.52px 0";
+      pInp = document.querySelector(".listText p");
+      if ((inp.value.length / pInp.offsetWidth) * 100 > 14) {
+        p.style.overflowX = "scroll";
+      }
+      
+      inp.value = "";
+      btnAdd.addEventListener("click", () => {
+        inp_list.style.display = "block";
+        inp_list.style.display = "flex";
+        btnClear.style.justifyContent = "center";
+        inp.focus();
+      });
+      if (ul_div.children.length > 5) {
+        ul_div.style.overflowY = "scroll";
+        ul_div.scrollTop = ul_div.scrollHeight;
+        ul_div.style.paddingTop = "5px";
+        ul_div.style.paddingBottom = "0px";
+      }
+      sortImg.addEventListener("click", sortList);
+      deleteList();
+      addDragDrop();
+      styleFunction();
+    }
+  }
+});
+function styleFunction() {
+  inp_list.style.display = "none";
 
-// reverse sort
-let ust =document.querySelector(".ust")
-ust.style.display="none"
+  inp.focus();
+}
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+}
+function dragEnter() {
+  this.classList.add("over");
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+function dragOver(e) {
+  e.preventDefault();
+}
 
-
-ust.addEventListener('click', () => {
-    alt.style.display="block" 
-    ust.style.display="none" 
-    let task=document.querySelectorAll('.task')
-    let y = [];
-    
-    task.forEach(ele =>{
-        y.push(ele.firstElementChild.innerText)
-    })
-    
-    tasks.innerHTML=""
-    y.reverse()
-    
-    y.forEach(item=>{
-        
-       tasks.innerHTML+= `<div class="task">
-       <label>
-        <input type="checkbox">
-        <span id="taskname">
-            ${item}
-        </span>
-        </label>
-        <button class="delete">x</button>
-    </div>`
-document.querySelector('.input').value=""
-let cancel = document.querySelectorAll(".delete");
- for(let i=0; i<cancel.length; i++){
-     cancel[i].onclick = function(){
-         this.parentNode.remove();
-     }}
-    
-    })
-    })
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove("over");
+}
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".listText");
+  const itemTwo = listItems[toIndex].querySelector(".listText");
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+function addDragDrop() {
+  const draggables = document.querySelectorAll(".liDiv");
+  const dragListItems = document.querySelectorAll(".ulDiv li");
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+}
+function sortList() {
+  arrSorting = [];
+  list = document.querySelectorAll(".listText p");
+  list.forEach((item) => {
+    arrSorting.push(item.innerHTML);
+  });
+  arrSorting.sort();
+  for (i = 0; i < list.length; i++) {
+    list[i].innerHTML = arrSorting[i];
+  }
+  sortImg.style.display = "none";
+  reverseImg.style.display = "block";
+  reverseImg.addEventListener("click", reverseList);
+}
+function reverseList() {
+  arrSorting = [];
+  list = document.querySelectorAll(".listText p");
+  list.forEach((item) => {
+    arrSorting.push(item.innerHTML);
+  });
+  arrSorting.sort().reverse();
+  for (i = 0; i < list.length; i++) {
+    list[i].innerHTML = arrSorting[i];
+  }
+  sortImg.style.display = "block";
+  reverseImg.style.display = "none";
+}
+function deleteList() {
+  btnDel = document.querySelectorAll(".deleteButton");
+  btnDel.forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+      item.style.background = "purple";
+      item.style.color = "white";
+      item.style.border = "none";
+    });
+    item.addEventListener("mouseout", (event) => {
+      item.style.background = "white";
+      item.style.color = "#C4C4C4";
+      item.style.border = "1px solid #C4C4C4";
+    });
+  });
+  for (var i = 0; i < btnDel.length; i++) {
+    btnDel[i].addEventListener("click", deleteListElement);
+  }
+}
+function deleteListElement() {
+  this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+  if (ul_div.children.length == 0) {
+    inp_list.style.display = "block";
+    inp_list.style.display = "flex";
+    ul_div.style.padding = "0";
+    inp.focus();
+  }
+  if (ul_div.children.length < 6) {
+    ul_div.style.overflowY = "hidden";
+  }
+}
